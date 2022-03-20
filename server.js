f4fc72d982a5b0ce81195a5f0412cf435e3af7d7
iTakedown27 <ezrabridger27@gmail.com>
2022-03-20 16:49:16 -0700

    added navbar to all pages

--- server.js
+++ server.js
@@ -8,14 +8,6 @@ app.get('/', (req, res) => {
     res.send('Up and running!');
 });
 
-app.get('/about', (req, res) => {
-    res.redirect('localhost:8080/about');
-});
-
-app.post('/', (req, res) => {
-    
-});
-
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
