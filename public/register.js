0d07bb99d181b9489373ec7479e7a8bbc9abf3f3
iTakedown27 <ezrabridger27@gmail.com>
2022-03-20 15:41:08 -0700

    login

--- public/register.js
+++ public/register.js
@@ -1,4 +1,27 @@
+const fs = require('fs')
+
 let users = [];
 let username = document.getElementById('input_username');
 let password = document.getElementById('input_password');
 
+class User {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    username() {
+        return this.username;
+    }
+
+    password() {
+        return this.password;
+    }
+}
+
+function addStudent(username, password) {
+    let newUser = new User(username, password);
+    users.push(newUser);
+    fs.writeFileSync('usernames.txt', username);
+    fs.writeFileSync('passwords.txt', password);
+}
