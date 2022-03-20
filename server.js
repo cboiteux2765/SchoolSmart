dec51dc5083b10b97d4b1c4b5d77d535127ffaac
iTakedown27 <ezrabridger27@gmail.com>
2022-03-20 16:15:18 -0700

    more stuff

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
 const port = process.env.PORT || 3000;
 
-const indexRouter = require('./routes/index');
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout'); // Stores all files
-app.use(expressLayouts);
 app.use(express.static('public'));
 
-app.use('/', indexRouter);
+app.get('/', (req, res) => {
+    res.redirect('./public/homepage.html');
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
